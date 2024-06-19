ba2d0f18a46f077dc8c58e3902dbb030936cf0ec
Aldo Cortesi <aldo@corte.si>
2024-06-19 17:54:38 +1200

    Initial import of msgpack-rpc

---
+++ crates/nvi-rpc/src/lib.rs
@@ -0,0 +1,14 @@
+//! This crate provides facilities to use the `MessagePack` remote procedure call system
+//! (`MessagePack-RPC`) in Rust.
+
+#[macro_use]
+extern crate log;
+
+mod codec;
+mod endpoint;
+mod errors;
+pub mod message;
+
+pub use crate::endpoint::{serve, Ack, Client, Endpoint, Response, Service, ServiceWithClient};
+pub use crate::errors::DecodeError;
+pub use rmpv::{Integer, Utf8String, Value};
