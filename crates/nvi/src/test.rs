e61fd4631ef6bb376e29945a132345abce520346
Aldo Cortesi <aldo@corte.si>
2024-12-29 15:25:59 +1300

    test: Improve formatting of logs

--- crates/nvi/src/test.rs
+++ crates/nvi/src/test.rs
@@ -99,12 +99,13 @@ impl NviTest {
                 }
                 Writer(logs)
             })
-            .with_ansi(false)
+            .with_ansi(true)
+            .without_time()
             .with_file(true)
             .with_line_number(true)
-            .with_thread_ids(true)
-            .with_thread_names(true)
-            .with_target(true)
+            .with_thread_ids(false)
+            .with_thread_names(false)
+            .with_target(false)
             .compact()
             .set_default();
 
