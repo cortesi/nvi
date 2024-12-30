298ec9376c1aff2d24db1bf28aae8dedf6f5714f
Aldo Cortesi <aldo@corte.si>
2024-12-30 21:40:58 +1300

    demos: use async functions

--- crates/nvi/src/demo.rs
+++ crates/nvi/src/demo.rs
@@ -4,7 +4,9 @@ use crate::error::Result;
 use crate::{client::Client, NviPlugin};
 
 /// A function that can be registered with the Demo struct.
-pub type DemoFunction = Box<dyn Fn(&crate::client::Client) -> crate::error::Result<()>>;
+pub type DemoFunction = Box<
+    dyn Fn(&crate::client::Client) -> futures::future::BoxFuture<'static, crate::error::Result<()>>,
+>;
 
 /// Holds a collection of named demo functions that can be executed with a Client.
 #[derive(Default)]
@@ -28,11 +30,13 @@ impl Demos {
     }
 
     /// Adds a named function to the demo collection.
-    pub fn add<F>(&mut self, name: impl Into<String>, f: F)
+    pub fn add<F, Fut>(&mut self, name: impl Into<String>, f: F)
     where
-        F: Fn(&Client) -> Result<()> + 'static,
+        F: Fn(&Client) -> Fut + 'static,
+        Fut: futures::Future<Output = Result<()>> + Send + 'static,
     {
-        self.functions.insert(name.into(), Box::new(f));
+        self.functions
+            .insert(name.into(), Box::new(move |client| Box::pin(f(client))));
     }
 
     /// Run a named demo function with a plugin instance.
@@ -48,7 +52,7 @@ impl Demos {
             .functions
             .get(name)
             .ok_or_else(|| crate::error::Error::Plugin(format!("no such demo: {}", name)))?;
-        f(&t.client)?;
+        f(&t.client).await?;
         t.finish().await?;
         Ok(())
     }
@@ -57,16 +61,40 @@ impl Demos {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::NviPlugin;
 
-    #[test]
-    fn test_demos() {
+    #[derive(Default)]
+    struct TestPlugin;
+    impl NviPlugin for TestPlugin {
+        fn name(&self) -> String {
+            "test".into()
+        }
+    }
+
+    #[tokio::test]
+    async fn test_demos() {
         let mut d = Demos::new();
         assert!(d.list().is_empty());
 
-        d.add("two", |_| Ok(()));
-        d.add("one", |_| Ok(()));
+        d.add("two", |c| {
+            let c = c.clone();
+            async move {
+                c.lua("print('demo two')").await?;
+                Ok(())
+            }
+        });
+        d.add("one", |c| {
+            let c = c.clone();
+            async move {
+                c.lua("print('demo one')").await?;
+                Ok(())
+            }
+        });
 
         let lst = d.list();
         assert_eq!(lst, vec!["one", "two"]);
+
+        // Test actually running a demo
+        d.run("one", TestPlugin).await.unwrap();
     }
 }
